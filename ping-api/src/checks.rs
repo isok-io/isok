cd3cb89640740cc0aa41588cd58450b42ffba152
Jean-Baptiste Kaiser <jean-baptiste.kaiser@clever-cloud.com>
2023-12-18 12:02:40 +0100

    add route boilerplate

---
+++ ping-api/src/checks.rs
@@ -0,0 +1,22 @@
+
+pub mod api {
+    pub async fn list_checks() {
+        todo!()
+    }
+
+    pub async fn get_check() {
+        todo!()
+    }
+
+    pub async fn create_check() {
+        todo!()
+    }
+
+    pub async fn update_check() {
+        todo!()
+    }
+
+    pub async fn delete_check() {
+        todo!()
+    }
+}
--- ping-api/src/main.rs
+++ ping-api/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+pub mod routes;
+pub mod auth;
+pub mod users;
+pub mod checks;
+
+#[tokio::main]
+async fn main() {
+    let app = routes::app();
+
+    let listener =
+        tokio::net::TcpListener::bind("localhost:8080").await.expect("Unable to bind");
+
+    axum::serve(listener, app).await.unwrap()
 }
