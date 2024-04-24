a743c6a4bc072e13dbc9bbc0a4499153e3938129
arendsyl <jbkaiser22@gmail.com>
2024-04-24 20:22:39 +0200

    remove log

--- ping-agent/src/sink.rs
+++ ping-agent/src/sink.rs
@@ -6,10 +6,6 @@ use ping_data::pulsar_messages::CheckMessage;
 
 pub async fn pulsar_sink(mut producer: Producer<TokioExecutor>, mut receiver: Receiver<CheckMessage>) {
     while let Some(check_msg) = receiver.recv().await {
-        info!(
-            "received a message of id: {}", check_msg.check_id
-        );
-
         producer.send(check_msg).await;
     }
 }
