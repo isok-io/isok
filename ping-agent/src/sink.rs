06a110ddf7633111389b6c4b7b39801bc83e3291
arendsyl <jbkaiser22@gmail.com>
2024-04-24 20:09:17 +0200

    add log in sink

--- ping-agent/src/sink.rs
+++ ping-agent/src/sink.rs
@@ -1,3 +1,4 @@
+use log::info;
 use pulsar::{Producer, TokioExecutor};
 use tokio::sync::mpsc::Receiver;
 
@@ -5,6 +6,10 @@ use ping_data::pulsar_messages::CheckMessage;
 
 pub async fn pulsar_sink(mut producer: Producer<TokioExecutor>, mut receiver: Receiver<CheckMessage>) {
     while let Some(check_msg) = receiver.recv().await {
+        info!(
+            "received a message of id: {}", check_msg.check_id
+        );
+
         producer.send(check_msg).await;
     }
 }
