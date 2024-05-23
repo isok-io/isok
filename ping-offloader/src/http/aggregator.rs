3fe7f9a8b706926c59df43bc3732a8831cc535af
arendsyl <jbkaiser22@gmail.com>
2024-05-23 18:32:09 +0200

    add pulsar sink for aggregated data

--- ping-offloader/src/http/aggregator.rs
+++ ping-offloader/src/http/aggregator.rs
@@ -1,6 +1,9 @@
 use std::cmp::max;
 use std::collections::{HashMap, HashSet};
 use std::time::Duration;
+use pulsar::producer::Message;
+use pulsar::{DeserializeMessage, Error, Payload, Producer, SerializeMessage, TokioExecutor};
+use serde::{Deserialize, Serialize};
 use time::{OffsetDateTime};
 use tokio::sync::broadcast::Receiver;
 use tokio::sync::mpsc::Sender;
@@ -9,11 +12,32 @@ use ping_data::check_kinds::http::HttpFields;
 use ping_data::pulsar_messages::{CheckData, CheckMessage, CheckResult};
 
 
+#[derive(Serialize, Deserialize)]
 pub struct AggregatedCheckMessage {
     check_id: Uuid,
     timestamp: OffsetDateTime,
     latency: Duration,
     status_codes: StatusCodeCount,
+}
+
+impl SerializeMessage for AggregatedCheckMessage {
+    fn serialize_message(input: Self) -> Result<Message, Error> {
+        let payload = serde_json::to_vec(&input).map_err(|e| Error::Custom(e.to_string()))?;
+
+        Ok(Message {
+            payload,
+            ..Default::default()
+        })
+    }
+}
+
+impl DeserializeMessage for AggregatedCheckMessage {
+    type Output = Result<Self, pulsar::Error>;
+
+    fn deserialize_message(payload: &Payload) -> Self::Output {
+        serde_json::from_slice(payload.data.as_slice())
+            .map_err(|e| pulsar::Error::Custom(e.to_string()))
+    }
 }
 
 pub struct AggregateValues {
@@ -43,7 +67,7 @@ impl Default for AggregateValues {
     }
 }
 
-#[derive(Default, Copy, Clone)]
+#[derive(Default, Copy, Clone, Serialize, Deserialize)]
 pub struct StatusCodeCount {
     pub _200: usize,
     pub _300: usize,
@@ -74,18 +98,27 @@ impl AggregateBuffer {
 
 pub struct Aggregator {
     http_receiver: Receiver<CheckData<HttpFields>>,
-    sender: Sender<AggregatedCheckMessage>,
+    pulsar_sink: Producer<TokioExecutor>,
     buffer: HashMap<Uuid, AggregateBuffer>,
 }
 
 impl Aggregator {
+    pub fn new(http_receiver: Receiver<CheckData<HttpFields>>,
+               pulsar_sink: Producer<TokioExecutor>) -> Self {
+        Self {
+            http_receiver,
+            pulsar_sink,
+            buffer: Default::default(),
+        }
+    }
+
     pub async fn run(&mut self) {
         while let Some(check_data) = self.http_receiver.recv().await.ok() {
             if let Some(check_buffer) = self.buffer.get_mut(&check_data.check_id) {
                 if !check_buffer.responded_agents.contains(&check_data.agent_id) {
                     check_buffer.add_check(&check_data)
                 } else {
-                    let _ = self.sender.send(
+                    let _ = self.pulsar_sink.send(
                         AggregatedCheckMessage {
                             check_id: check_data.check_id,
                             timestamp: check_buffer.timestamp,
--- ping-offloader/src/http/mod.rs
+++ ping-offloader/src/http/mod.rs
@@ -1,13 +1,43 @@
+use futures::FutureExt;
 use log::{error, info};
+use pulsar::{Authentication, Producer, Pulsar, TokioExecutor};
 use tokio::sync::broadcast;
 use ping_data::check_kinds::http::HttpFields;
 use ping_data::pulsar_messages::CheckType;
 use crate::{env_get, env_get_num};
+use crate::http::aggregator::{AggregatedCheckMessage, Aggregator};
 use crate::http::warp10::{Warp10Client, Warp10ConnectionData, Warp10HttpSink};
 use crate::pulsar_source::{pulsar_http_topic, PulsarConnectionData, PulsarSource};
 
 pub mod warp10;
 pub mod aggregator;
+
+pub fn pulsar_producer_topic(connection_data: &PulsarConnectionData) -> String {
+    format!(
+        "persistent://{}/{}/{}",
+        connection_data.pulsar_tenant,
+        connection_data.pulsar_namespace,
+        "aggregated-http"
+    )
+}
+
+pub async fn pulsar_http_aggregate_sink(connection_data: PulsarConnectionData) -> Option<Producer<TokioExecutor>> {
+    let client = Pulsar::builder(&connection_data.pulsar_address, TokioExecutor)
+        .with_auth(Authentication {
+            name: "token".to_owned(),
+            data: Vec::from(connection_data.pulsar_token.as_bytes()),
+        })
+        .build()
+        .await
+        .ok()?;
+
+    client.producer()
+        .with_topic(pulsar_producer_topic(&connection_data))
+        .with_name("aggregated-http".to_string())
+        .build()
+        .await
+        .ok()
+}
 
 pub async fn run_http(pulsar_connection_data: PulsarConnectionData) {
     let warp10_address = env_get("WARP10_ADDRESS");
@@ -24,12 +54,25 @@ pub async fn run_http(pulsar_connection_data: PulsarConnectionData) {
         warp10_receiver) =
         broadcast::channel(channel_capacity);
 
+    let aggregator_receiver = pulsar_sender.subscribe();
+
     info!(
         "Connecting to pulsar topic {}...",
         pulsar_http_topic(&pulsar_connection_data)
     );
 
     let mut pulsar_source: PulsarSource<HttpFields> = match PulsarSource::new(&pulsar_connection_data, pulsar_sender, CheckType::Http).await {
+        Some(pc) => {
+            info!("Connected to pulsar topic !");
+            pc
+        }
+        None => {
+            error!("Failed to connect to pulsar topic");
+            std::process::exit(1);
+        }
+    };
+
+    let pulsar_aggregate_sink: Producer<TokioExecutor> = match pulsar_http_aggregate_sink(pulsar_connection_data).await {
         Some(pc) => {
             info!("Connected to pulsar topic !");
             pc
@@ -54,6 +97,9 @@ pub async fn run_http(pulsar_connection_data: PulsarConnectionData) {
 
     let warp10_http_sink = Warp10HttpSink::new(warp10_client, warp10_receiver);
 
+    let mut aggregator_http_sink = Aggregator::new(aggregator_receiver, pulsar_aggregate_sink);
+
     pulsar_source.run().await;
     warp10_http_sink.run().await;
+    aggregator_http_sink.run().await;
 }
